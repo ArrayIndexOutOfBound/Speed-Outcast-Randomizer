0b3b258b20d522e422969062a2a44b321a711890
Mugsworth1 <jonathan.wilcock@hotmail.co.uk>
2025-01-08 22:35:25 +0000

    Add a new top menu section for Randomizer, add new 'Basic' and 'Enhancements' submenus, add settings for randomizer enhancements.

---
+++ assets/Bundle Assets.ps1
@@ -0,0 +1,13 @@
+﻿if(Get-Item assets_randomizer.pk3)
+{
+    Remove-Item assets_randomizer.pk3
+}
+
+$compress = @{
+  Path = "ui", "models"
+  CompressionLevel = "Fastest"
+  DestinationPath = "assets_randomizer.zip"
+}
+Compress-Archive @compress
+
+Move-Item assets_randomizer.zip assets_randomizer.pk3
